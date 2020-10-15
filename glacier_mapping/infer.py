1aaa25af161b4980139fda37bde77738f43c19a5
Kris Sankaran <sankaran.kris@gmail.com>
2020-10-15 17:05:15 -0500

    no need to merge

--- glacier_mapping/infer.py
+++ glacier_mapping/infer.py
@@ -62,77 +62,8 @@ def predict_tiff(path, model, subset_size=None, conf_path="conf/postprocess.yaml
         img = imgf.read(window=Window(0, 0, subset_size[0], subset_size[1]))
     else:
         img = imgf.read()
-    x, y_hat = inference(img, model, conf_path)
+    x, y_hat = run_model_on_tile(img, model)
     return img, x, y_hat
-
-
-def merge_patches(patches, overlap):
-    I, J, _, height, width, channels = patches.shape
-    result = np.zeros((I * height, J * width, channels))
-    for i in range(I):
-        for j in range(J):
-            ix_i = i * (height - overlap)
-            ix_j = j * (width - overlap)
-            result[ix_i : (ix_i + height), ix_j : (ix_j + width)] = patches[i, j]
-
-    return result
-
-
-def inference(img, model, process_conf, overlap=0, infer_size=1024, device=None):
-    """Make predictions on an unprocessed tiff
-
-    :param img: A (unprocessed) numpy array on which to do inference.
-    :type img: np.array
-    :param model: A pytorch model on which to perform inference. We assume it
-      can accept images of size specified in process_conf.slice.size.
-    :type model: pytorch.nn.Module
-    :param process_conf: The path to a yaml file giving the postprocessing
-      options. Used to convert the raw tile into the tensor used for inference.
-    :type process_conf: string
-    :param overlap: The number of overlapping pixels when splitting the patches
-      on which to apply the model.
-    :type overlap: int
-    :param infer_size: The size of the square on which to perform inference.
-    :type infer_size: int
-    :param device: The device (gpu or cpu) on which to run inference.
-    :type device: torch.device
-    :return prediction: A segmentation mask of the same width and height as img.
-    :type prediction: np.array
-    """
-    if device is None:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # reshape, pad, and slice the input
-    size_ = img.shape
-    img = pad_to_valid(img)
-    img = np.transpose(img, (1, 2, 0))
-    slice_size = (
-        min(img.shape[0], infer_size),
-        min(img.shape[1], infer_size),
-        img.shape[2]
-    )
-    slice_imgs = view_as_windows(img, slice_size, step=slice_size[0] - overlap)
-
-    I, J, _, _, _, _ = slice_imgs.shape
-    predictions = np.zeros((I, J, 1, slice_size[0], slice_size[1], 1))
-    patches = np.zeros((I, J, 1, slice_size[0], slice_size[1], len(channels)))
-
-    for i in range(I):
-        for j in range(J):
-            patch = slice_imgs[i, j, 0]
-            patches[i, j, :] = patch
-            patch = np.transpose(patch, (2, 0, 1))
-            patch = torch.from_numpy(patch).float().unsqueeze(0)
-
-            with torch.no_grad():
-                patch = patch.to(device)
-                y_hat = model(patch).cpu().numpy()
-                y_hat = 1 / (1 + np.exp(-y_hat))
-                predictions[i, j, 0] = np.transpose(y_hat, (0, 2, 3, 1))
-
-    x = merge_patches(patches, overlap)
-    y_hat = merge_patches(predictions, overlap)
-    return x[:size_[1], :size_[2], :], y_hat[:size_[1], :size_[2], :]
 
 
 def run_model_on_tile(tile, model, device=None, batch_size=256, num_output_channels=3,
@@ -184,23 +115,6 @@ def run_model_on_tile(tile, model, device=None, batch_size=256, num_output_chann
 
     output = output / counts[..., np.newaxis]
     return output
-
-
-
-
-
-
-def next_multiple(size):
-    return np.ceil(size / 512) * 512
-
-
-def pad_to_valid(img):
-    size_ = img.shape
-    out_rows = next_multiple(size_[1])
-    out_cols = next_multiple(size_[2])
-
-    pad_shape = (int(out_rows - size_[1]), int(out_cols - size_[2]))
-    return np.pad(img, ((0, 0), (0, pad_shape[0]), (0, pad_shape[1])))
 
 
 def convert_to_geojson(y_hat, bounds, threshold=0.8):
