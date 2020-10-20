fd10ab3f905e68e0046b5f14fd7512e7291c329b
Kris Sankaran <sankaran.kris@gmail.com>
2020-10-20 17:32:03 -0500

    for now, skip reprojection

--- web/prepare_backend.sh
+++ web/prepare_backend.sh
@@ -10,10 +10,14 @@ source .env
 export WEB_DIR=$DATA_DIR/web_data/
 
 # data prep for backend
-python3 -m web.backend.backend_data -d $DATA_DIR/analysis_images/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-full.vrt # tiles/ directory must exist
-python3 -m web.backend.backend_data -d $DATA_DIR/analysis_images/ -o $ROOT_DIR/web/frontend/outputs/tiles/ -n output-245.vrt --bandList 5 4 2
+python3 -m web.backend.backend_data -d $DATA_DIR/analysis_images/ -o $ROOT_DIR/web/frontend/outputs/basemap/ -n output-full.vrt # tiles/ directory must exist
+python3 -m web.backend.backend_data -d $DATA_DIR/analysis_images/ -o $ROOT_DIR/web/frontend/outputs/basemap/ -n output-245.vrt --bandList 5 4 2
 python3 -m web.backend.backend_data -d $WEB_DIR/processed/preds/ -o $ROOT_DIR/web/frontend/outputs/pred_tiles/ -n y_hat.vrt --bandList 1 --tile True # tiles/ directory must exist
 
 # copy tile outputs to $ROOT_DIR/web/outputs/
 # you will see results at http://0.0.0.0:4040/web/frontend/index.html
 python3 -m web.backend.server & python3 -m web.frontend_server
+
+
+python3 -m web.backend.backend_data -d $DATA_DIR/analysis_images/ -o $ROOT_DIR/web/frontend/outputs/basemap/ -n output-full.vrt # tiles/ directory must exist
+python3 -m web.backend.backend_data -d $DATA_DIR/analysis_images/ -o $ROOT_DIR/web/frontend/outputs/basemap/ -n output-245.vrt --bandList 5 4 2
