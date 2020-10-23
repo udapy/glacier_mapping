06da13e0dc4bd54204e37af87a15381f6213838c
Kris Sankaran <sankaran.kris@gmail.com>
2020-10-22 19:56:29 -0500

    try giving file:// path to tiles

--- web/prepare_backend.sh
+++ web/prepare_backend.sh
@@ -4,13 +4,12 @@
 source .env
 
 # if data or directories don't exist, you'll need to fill them in
-# mkdir -p $WEB_DIR/processed/preds/
 # mkdir -p $DATA_DIR/web/basemap/
-export WEB_DIR=$DATA_DIR/web_data/
+mkdir -p $ROOT_DIR/web/basemap/
 
 # data prep for backend
-python3 -m web.backend.backend_data -d $DATA_DIR/unique_tiles/ -o $DATA_DIR/web/basemap2/ -n output-full.vrt;
-python3 -m web.backend.backend_data -d $DATA_DIR/unique_tiles/ -o $DATA_DIR/web/basemap2/ -n output-245.vrt --bandList 5 4 2
+python3 -m web.backend.backend_data -d $DATA_DIR/web_data/img_data/ -o $DATA_DIR/web/basemap/ -n output-full.vrt;
+python3 -m web.backend.backend_data -d $DATA_DIR/web_data/img_data/ -o $DATA_DIR/web/basemap/ -n output-245.vrt --bandList 5 4 2
 
 cd $DATA_DIR/web/basemap/
 gdal_translate -ot Byte output-245.vrt output-245-byte.vrt
@@ -18,9 +17,9 @@ gdal_translate -ot Byte output-245.vrt output-245-byte.vrt
 for i in $( seq 8 14 )
 do
     gdal2tiles.py -z $i --processes 25 output-245-byte.vrt .
+    cp -r $i $ROOT_DIR/web/basemap/
 done;
 
 # copy tile outputs to $ROOT_DIR/web/outputs/
-# mkdir -p $ROOT_DIR/web/outputs/tiles/
 # you will see results at http://0.0.0.0:4040/web/frontend/index.html
 python3 -m web.frontend_server & python3 -m web.backend.server
